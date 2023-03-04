a39a19806ed665bd38ecaca226051caebff1411a
thuongtrant <115720214+thuongtrant@users.noreply.github.com>
2023-03-04 14:23:55 +0700

    Update BT1.cpp

--- Pointer/Pointer/BT1.cpp
+++ Pointer/Pointer/BT1.cpp
@@ -7,7 +7,7 @@
 //	p = &x;
 //	*p = 80;
 //	cout << "Xuat dia chi cua bien x: " << &x << endl;
-//	cout << "Xuat dia chi luu trong con tro p: " << &p << endl;
+//	cout << "Xuat dia chi luu trong con tro p: " << p << endl;
 //	cout << "Xuat gia tri cua bien ma p tro den: " << *p << endl;
 //	cout << "Xuat gia tri cua bien x: " << x << endl;
 //	return 0;
